343d357b065088affdb440c5dc376d94f8caa3fb
ArnabDey1543 <arnabdey1543@gmail.com>
2024-01-27 15:24:36 +0530

    [Arnab] Modified: regter method is modifed

--- routes/register.js
+++ routes/register.js
@@ -5,13 +5,20 @@ const Joi = require("joi");
 
 router.post("/", async (req, res) => {
 	try {
-		// const { error } = validate(req.body);
-		// if (error)
-		// 	return res.status(400).send({ message: error.details[0].message });
+		const { error } = validate(req.body);
+		if (error)
+			return res.status(400).send({ message: error.details[0].message });
+
+
+
 
 		const user = await User.findOne({ email: req.body.email });
 		if (!user)
 			return res.status(401).send({ message: "Invalid Email or Password" });
+
+
+
+
 
 		const validPassword = await bcrypt.compare(
 			req.body.password,
@@ -22,8 +29,12 @@ router.post("/", async (req, res) => {
 
 		const token = user.generateAuthToken();
 		res.status(200).send({ data: token, message: "logged in successfully" });
+
+
+
+
 	} catch (error) {
-		res.status(500).send({ message: "Internal Server Error" });
+		res.status(500).send({ message: "Server Error" });
 	}
 });
 
